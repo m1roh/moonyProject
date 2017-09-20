035c6464e0a58c00f14c2c7dfbe09b3bbfeb0ba2
wilfried orsi <orsi.wilfried@gmail.com>
2017-09-20 09:54:07 +0200

    ajout Nos engagement

---
+++ charte.js
@@ -0,0 +1,12 @@
+$("#image1").hover(function() {
+    $("#image .p1").toggle()
+    
+  });
+
+  $().hover(function() {
+    $(".p1").toggle();
+   })
+
+  $("#acrobalance img ").hover(function() {
+   $("#acrobalance .p").toggle();
+})
